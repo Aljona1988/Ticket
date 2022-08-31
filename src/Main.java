d53a40be1e593a233b641cfc2edd9d072517e09e
Elena Fedkina <zhrulick@maiil.ru>
2022-09-01 00:26:46 +0300

    add .gitignore

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 100; // Стоимость билета
+        int amount = 20;// Стоимость 1 мили
+        int mile = ticket / amount ;// Расчет миль
+
+        System.out.println ("Количество миль" + mile);
+
+
+
+
+
+    }
+}
