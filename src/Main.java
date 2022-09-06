c654b71f8e555be0995b5f426714bdf43140a153
Elena Fedkina <zhrulick@maiil.ru>
2022-09-06 23:12:43 +0300

    add calculate

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-
-        int ticket = 100; // Стоимость билета
-        int amount = 20;// Стоимость 1 мили
-        int mile = ticket / amount ;// Расчет миль
-
-        System.out.println ("Количество миль" + mile);
-
-
-
-
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
+
+
+
+
+
